04a7027b26606caab3929458c12ea5f593468553
Sylvia Maina <sylviamaina16@gmail.com>
2023-01-09 06:41:21 +0300

    try

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Layout } from "../components/Index";
+import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import styles from "../styles/Home.module.css";
