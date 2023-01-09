f7fad0303f1d28d3f7679f73ac9083238daf7d60
Sylvia Maina <sylviamaina16@gmail.com>
2023-01-09 16:30:11 +0300

    try

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getPosts, getPostDetails } from "../../services";
+import { getPosts, getPostDetails } from "../../Services";
 import {
   PostDetail,
   Categories,
@@ -8,7 +8,7 @@ import {
   Comments,
   CommentsForm,
   Loader,
-} from "../../components";
+} from "../../Components";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
