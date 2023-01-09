f86b03a847cb88a9f753974a77bc2bc1a9ca9918
Sylvia Maina <sylviamaina16@gmail.com>
2023-01-09 15:34:56 +0300

    upper

---
+++ Components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Header} from "./index"
+
+
+const Layout = ({children}) => {
+  return (
+    <>
+        <Header />
+        {children}
+    </>
+  )
+}
+
+export default Layout
